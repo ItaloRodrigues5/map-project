7ca1e0feb63b58029d830c443c731d6bd30c046c
carloseduardo002 <eduafrat@gmail.com>
2025-09-19 21:23:13 -0300

    serializer

--- myapp/api/v1/serializers.py
+++ myapp/api/v1/serializers.py
@@ -1,8 +1,19 @@
 from rest_framework import serializers
 from myapp.models import Client
-
+from myapp.models import Veiculo
+from myapp.models import Venda
 
 class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
-        fields = '__all__'
+        fields = '_all_'
+
+class VeiculoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Veiculo
+        fields = '_all_'
+
+class VendaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Venda
+        fields = '_all_'
